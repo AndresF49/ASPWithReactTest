88a019e52755368130b2f8a3b63d35a11421fba1
Andres F <andres06037@gmail.com>
2023-10-18 17:23:22 -0500

    These are the changes I made to add a new Page called Login, edited navbar to link to the component

---
+++ ClientApp/src/components/Account/Login.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+
+export class Login extends Component {
+    static displayName = Login.name;
+
+    render() {
+        return (
+            <div>
+                <h2 class="text-center mt-5">Hello, from Login page!</h2>
+                <div class="row">
+                    <div class="col-md-6 offset-md-3">
+                        This is where login info and input fields will be
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
